ea6edd63d82566cbef312c62c47ce44df7f7a94d
Yuki Nozaki <nq127main@gmail.com>
2018-01-29 13:15:51 +0900

    これからbfs書く

--- atCoder/ARC/90/D/main.cpp
+++ atCoder/ARC/90/D/main.cpp
@@ -1,24 +1,39 @@
 #include <iostream>
-#include <map>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-int main() {
-   map<int, map<int, int> > mp;
-   map<int, pair<int, int> > inputs;
+vector<vector<int> > mp;
 
+int bfs(int st, int end, vector<int> used_list){
+ 
+}
+
+int main() {
    int N, M;
    cin >> N >> M;
+   mp.resize(N);
+   for(int i=0; i<N; i++){
+     mp[i].resize(N);
+     for(int j=0; j<N; j++){
+       mp[i][j] = -1;
+     }
+   }
    for(int i=0; i < M; i++){
      int l, r, d;
-     cin >> l, r, d;
-     inputs[l] = make_pair(r,d);
+     cin >> l >> r >> d;
+     l--; r--;
+     mp[l][r] = d;
+     mp[r][l] = -d;
    }
    
-   for(map<int,pair<int, int> >::iterator it=inputs.begin(); it != inputs.end(); ++it){
-     cout << it->first << " " << it->second.first << ":" << it->second.second << endl;
-
-   }
-   
+   // for debug
+   // for(auto i : mp){
+   //   for(auto j : i){
+   //     cout << j <<" ";
+   //   }
+   //   cout << endl;
+   // } 
    return 0;
 }
