949aeb099c9a36df7390baa3d665e231252ce922
Yuki Nozaki <yuki_nozaki@voyagegroup.com>
2018-09-10 07:05:51 +0900

    ABC 109 D の実装してみた
    AC はしたけどジグザグ走査がマジでそのまま

--- atCoder/ABC/109/D/D.py
+++ atCoder/ABC/109/D/D.py
@@ -9,20 +9,44 @@ for i in range(H):
     w =list(map(lambda x: int(x), input().strip().split())) 
     ahw.append(w)
 
-odds = []
+N = 0
+hasCoin = 0
+sousa = []
 
-for h in range(H):
-    for w in range(W):
-        if ahw[h][w] % 2 == 1:
-            odds.append((h, w))
+for i in range(H):
+    if i % 2 == 0:
+        for j in range(W):
+            if ahw[i][j] % 2 == 0:
+                continue
 
-distList = []
+            if not j == W - 1:
+                ahw[i][j] = ahw[i][j] - 1
+                ahw[i][j+1] = ahw[i][j+1] + 1
+                sousa.append("{} {} {} {}".format(i+1, j+1, i+1 , j+2))
+                N += 1
+            else:
+                if i != H - 1:
+                    ahw[i][j] = ahw[i][j] - 1
+                    ahw[i+1][j] = ahw[i+1][j] + 1
+                    sousa.append("{} {} {} {}".format(i+1, j+1, i+2 , j+1))
+                    N += 1
+    else:
+        for j in range(W - 1, -1, -1):
+            if ahw[i][j] % 2 == 0:
+                continue
 
-for val in odds:
-    dists = []
-    for odd in odds:
-        dists.append(pow((val[0]-odd[0]),2) + pow((val[1]-odd[1]),2))
-    distList.append(dists)
+            if not j == 0:
+                ahw[i][j] = ahw[i][j] - 1
+                ahw[i][j-1] = ahw[i][j-1] + 1
+                sousa.append("{} {} {} {}".format(i+1, j+1, i+1 , j))
+                N += 1
+            else:
+                if i != H - 1:
+                    ahw[i][j] = ahw[i][j] - 1
+                    ahw[i+1][j] = ahw[i+1][j] + 1
+                    sousa.append("{} {} {} {}".format(i+1, j+1, i+2 , j+1))
+                    N += 1
 
-print(distList)
-        
+print(N)
+for el in sousa:
+    print(el)
